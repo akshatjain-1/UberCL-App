952c116fd1b4b8092bbf6120e6ec50d99cca35a5
Akshat Jain <jnakshat202.com>
2025-01-03 23:06:32 +0530

    server setup

--- backend/app.js
+++ backend/app.js
@@ -4,14 +4,22 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const connectToDb = require('./db/db');
+const userRoutes = require('./routes/user.routes')
 
 connectToDb();
 
 app.use(cors());
+app.use(express.json())
+app.use(express.urlencoded({extense : true}));
 
 
 app.get('/' , (req,res) => { 
     res.send('hello you there?');
 });
+
+app.use(express.json());
+app.use('/users', userRoutes);
+
+
 
 module.exports=app;
