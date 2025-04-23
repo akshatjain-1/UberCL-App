4eaa930e776979925d023f94d50480191fe72280
Akshat Jain <jnakshat202@gmail.com>
2025-04-24 03:54:18 +0530

    .

--- backend/app.js
+++ backend/app.js
@@ -8,6 +8,7 @@ const connectToDb = require('./db/db');
 const userRoutes = require('./routes/user.routes')
 const captainRoutes = require('./routes/captain.routes');
 const mapRoutes = require('./routes/maps.routes');
+const rideRoutes = require('./routes/ride.routes');
 
 connectToDb();
 
@@ -25,6 +26,7 @@ app.use(express.json());
 app.use('/users', userRoutes);
 app.use('/captains', captainRoutes);
 app.use('/maps', mapRoutes);
+app.use('/rides', rideRoutes);
 
 
 
--- backend/routes/ride.routes.js
+++ backend/routes/ride.routes.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const router = express.Router();
 const {body} = require('express-validator');
+const rideModel = require('../models/ride.model');
+const rideController = require('../controllers/ride.controller');
+const authMiddleware = require('../middlewares/auth.middleware');
 
 router.post('/create',
+    authMiddleware.authUser,
     body('userID').isString().isLength({min: 24, max: 24}).withMessage('Invalid user ID'),
     body('pickup').isString().isLength({min: 3}).withMessage('Invalid pickup location'),
     body('dropoff').isString().isLength({min: 3}).withMessage('Invalid dropoff location'),
+    body('vehicleType').isString().isIn(['auto', 'car', 'motorcycle']).withMessage('Invalid vehicle type'),
+    rideController.createRide
 )
 
 
